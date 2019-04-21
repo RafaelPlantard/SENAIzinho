d4c70582b7e5e59e197aeba03cbfa4e2ad6259f1
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2019-04-21 12:21:40 -0300

    Improve Console.swift to work with menu option

--- Sources/SENAIzinho/Console.swift
+++ Sources/SENAIzinho/Console.swift
@@ -5,14 +5,27 @@
 //  Created by Rafael Ferreira on 4/21/19.
 //
 
-final class Console {
+import class Foundation.NSString
+
+enum Console {
     static func readLine<T>() -> T {
-        guard let line = Swift.readLine(), let castLine = line as? T else {
-            fatalError()
+        guard let line = Swift.readLine() else {
+            fatalError("Fail to read line")
+        }
+
+        guard let castLine = line as? T else {
+            fatalError("Fail to cast to \(T.self)")
         }
 
         return castLine
     }
+
+    static func readLine() -> Int {
+        let line: NSString = readLine()
+
+        return line.integerValue
+    }
+
 
     static func write(_ string: String) {
         print(string, separator: "", terminator: "")
--- Sources/SENAIzinho/main.swift
+++ Sources/SENAIzinho/main.swift
@@ -1,1 +1,10 @@
-print("Hello, world!")
+Console.writeLine("1. Cadastrar Aluno")
+Console.writeLine("2. Cadastrar Sala")
+Console.writeLine("3. Alocar Aluno")
+Console.writeLine("4. Remover Aluno")
+Console.writeLine("5. Verificar Salas")
+Console.writeLine("6. Verificar Alunos")
+Console.writeLine("0. Sair")
+Console.write("Escolha a opção desejada (0 - 6): ")
+
+let option: Int = Console.readLine()
