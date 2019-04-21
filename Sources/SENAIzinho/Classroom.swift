18ac6c410ce35dfa7f3a18d1cc406bed78a0fe82
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2019-04-21 12:21:58 -0300

    Add Student and Classroom structures

---
+++ Sources/SENAIzinho/Classroom.swift
@@ -0,0 +1,13 @@
+//
+//  Classroom.swift
+//  SENAIzinho
+//
+//  Created by Rafael Ferreira on 4/21/19.
+//
+
+struct Classroom {
+    let classroomNumber: Int
+    let currentCapacity: Int
+    let totalCapacity: Int
+    let students: [String]
+}
---
+++ Sources/SENAIzinho/Student.swift
@@ -0,0 +1,15 @@
+//
+//  Student.swift
+//  SENAIzinho
+//
+//  Created by Rafael Ferreira on 4/21/19.
+//
+
+import struct Foundation.Date
+
+struct Student {
+    let name: String
+    let birthday: Date
+    let course: String
+    let classroomNumber: Int
+}
